a729bdc1227e26d52e9e8677a905c2dbae5042ee
28-aditya <93944077+28-aditya@users.noreply.github.com>
2026-08-09 09:51:49 +0400

    added tree traversal search

--- storage/bPlusTree.go
+++ storage/bPlusTree.go
@@ -4,8 +4,8 @@ type bPlusTree struct {
 	rootNode *Node
 }
 
-func (currTree *bPlusTree) setRoot(root *Node) {
-	currTree.rootNode = root
+func (tree *bPlusTree) setRoot(root *Node) {
+	tree.rootNode = root
 }
 
 func NewTree() *bPlusTree {
@@ -16,4 +16,29 @@ func NewTree() *bPlusTree {
 	}
 
 	return &newTree
+}
+
+func (tree *bPlusTree) SearchTree(key int) (any, bool) {
+	curr := tree.rootNode
+
+	for {
+		if curr.IsLeaf {
+			for i, k := range curr.Keys {
+				if k == key {
+					return curr.Values[i], true
+				}
+			}
+			return nil, false
+		}
+
+		childIndex := len(curr.Pointers) - 1
+		for i, k := range curr.Keys {
+			if key < k {
+				childIndex = i
+				break
+			}
+		}
+
+		curr = curr.Pointers[childIndex]
+	}
 }
