e1c12d7d0e75a5efed101dec988f3365e554111f
28-aditya <93944077+28-aditya@users.noreply.github.com>
2026-08-11 17:35:06 +0400

    added data insertion without leaf splitting

--- storage/bPlusTree.go
+++ storage/bPlusTree.go
@@ -1,5 +1,8 @@
 package storage
 
+import ("errors")
+const KEYCAPACITY = 4
+
 type bPlusTree struct {
 	rootNode *Node
 }
@@ -18,9 +21,9 @@ func NewTree() *bPlusTree {
 	return &newTree
 }
 
-func (tree *bPlusTree) findLeaf(key int) (*Node) {
+func (tree *bPlusTree) findLeaf(key int) *Node {
 
-	curr:= tree.rootNode
+	curr := tree.rootNode
 
 	for {
 		if curr.IsLeaf {
@@ -40,16 +43,38 @@ func (tree *bPlusTree) findLeaf(key int) (*Node) {
 }
 
 func (tree *bPlusTree) SearchTree(key int) (any, bool) {
-	
+
 	leaf := tree.findLeaf(key)
 
 	if leaf != nil {
-		for i,k := range leaf.Keys {
+		for i, k := range leaf.Keys {
 			if k == key {
 				return leaf.Values[i], true
 			}
 		}
-	} 
+	}
 
 	return nil, false
+}
+
+func (tree *bPlusTree) Insert(key int, value any) error {
+	leaf := tree.findLeaf(key)
+
+	if len(leaf.Keys) >= KEYCAPACITY {
+		return errors.New("KEY CAPACITY EXCEEDED")
+	}
+
+	i := len(leaf.Keys)
+
+	for j, k := range leaf.Keys {
+		if k > key {
+			i = j
+			break
+		}
+	}
+	
+	leaf.Keys 	= 	append(leaf.Keys[:i], append([]int{key}, leaf.Keys[i:]...)...)
+	leaf.Values = 	append(leaf.Values[:i], append([]any{value}, leaf.Values[i:]...)...)
+	return nil
+	
 }
